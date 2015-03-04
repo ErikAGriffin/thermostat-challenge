01c120dae6d83d49e80d18e7935a7978ad0d794c
Erik Griffin <griffin.erik@gmail.com>
2015-03-04 11:26:54 +0000

    Finished basic Backend Model

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -6,8 +6,12 @@ describe("Thermostat", function() {
     thermostat = new Thermostat;
   });
 
-  it('should be initialized with a temperature of 20 C', function() {
+  it('is initialized with a temperature of 20 C', function() {
     expect(thermostat.temperature).toEqual(20);
+  });
+
+  it('is initialized with the PowerSaver ON', function() {
+    expect(thermostat.isPowerSaving).toBe(true);
   });
 
   describe('Changing Temperate', function() {
@@ -21,6 +25,51 @@ describe("Thermostat", function() {
       thermostat.lowerTemp();
       expect(thermostat.temperature).toEqual(19);
     });
+
+    it('The temperature will not lower beyond 10 degrees', function() {
+      for (i=0; i<11; i++) {
+        thermostat.lowerTemp();
+      }
+      expect(thermostat.temperature).toEqual(10);
+    });
+
+    describe('With and without PowerSaver', function() {
+
+      it('Can toggle PowerSaver OFF', function() {
+        thermostat.togglePowerSaver();
+        expect(thermostat.isPowerSaving).toBe(false);
+      });
+
+      it('Can toggle PowerSaver ON', function() {
+        thermostat.togglePowerSaver();
+        thermostat.togglePowerSaver();
+        expect(thermostat.isPowerSaving).toBe(true);
+      });
+
+      it('Will not raise above 25 if PowerSaver is ON', function() {
+        for (i=0; i<6; i++) {
+          thermostat.raiseTemp();
+        }
+        expect(thermostat.temperature).toEqual(25);
+      });
+
+      it('Will not raise above 32 if PowerSaver is OFF', function() {
+        thermostat.togglePowerSaver();
+        for (i=0;i<13;i++) {
+          thermostat.raiseTemp();
+        }
+        expect(thermostat.temperature).toEqual(32);
+      });
+
+
+    }); // end Describe 'With and Without PowerSaver'
+
+    it('Can reset the temperature to 20 with RESET', function() {
+      thermostat.raiseTemp();
+      thermostat.resetTemp();
+      expect(thermostat.temperature).toEqual(20);
+    });
+
 
   }); // end Describe 'Changing Temperature' temperature
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,11 +1,31 @@
 function Thermostat() {
   this.temperature = 20;
+  this.isPowerSaving = true;
+};
+
+Thermostat.prototype.togglePowerSaver = function() {
+  this.isPowerSaving = !this.isPowerSaving;
 };
 
 Thermostat.prototype.raiseTemp = function() {
-  this.temperature++;
+  if (this.isPowerSaving) {
+    if (this.temperature < 25) {
+      this.temperature++;
+    }
+  }
+  else {
+    if (this.temperature < 32) {
+      this.temperature++;
+    }
+  }
 };
 
 Thermostat.prototype.lowerTemp = function() {
-  this.temperature--;
+  if (this.temperature > 10) {
+    this.temperature--;
+  }
+};
+
+Thermostat.prototype.resetTemp = function () {
+  this.temperature = 20;
 };
