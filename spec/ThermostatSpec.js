6d9d925be673c2d0ca2cb413439cb296c78b6ba6
Erik Griffin <griffin.erik@gmail.com>
2015-03-04 10:56:29 +0000

    Can change temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -10,5 +10,19 @@ describe("Thermostat", function() {
     expect(thermostat.temperature).toEqual(20);
   });
 
+  describe('Changing Temperate', function() {
+
+    it('Pressing ↑ raises the temperature by 1 degree', function() {
+      thermostat.raiseTemp();
+      expect(thermostat.temperature).toEqual(21);
+    });
+
+    it('Pressing ↓ lowers the temperature by 1 degree', function() {
+      thermostat.lowerTemp();
+      expect(thermostat.temperature).toEqual(19);
+    });
+
+  }); // end Describe 'Changing Temperature' temperature
+
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,3 +1,11 @@
 function Thermostat() {
   this.temperature = 20;
 };
+
+Thermostat.prototype.raiseTemp = function() {
+  this.temperature++;
+};
+
+Thermostat.prototype.lowerTemp = function() {
+  this.temperature--;
+};
